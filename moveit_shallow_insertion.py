25bbbcd95af7539232e9db0217d7af30464f38d8
oliviaHKUST <38236454+oliviaHKUST@users.noreply.github.com>
2018-05-16 22:40:20 +0800

    add rotate about the axis function

--- moveit_shallow_insertion.py
+++ moveit_shallow_insertion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #Author: Olivia
-#Date: 2018/5/3
+#Date: 2018/5/15
 
 
 # Included headers
@@ -15,8 +15,108 @@ from tf import transformations as tf
 import numpy as np
 from math import *
 from std_msgs.msg import String
+from moveit_msgs.msg import RobotTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
 
 global gripper_command_publisher
+
+
+def calc_R_h_from_ksi(w_init, v_init, theta):
+  #MatrixXd w_cross_v_m(3, 1)
+  w_cross_v_m = np.matrix('0.0; 0.0; 0.0')
+  R_ksi_1st = np.matrix('0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0')
+  R_w_1st = np.matrix('0.0 0.0 0.0; 0.0 0.0 0.0; 0.0 0.0 0.0')
+  R_w_hat = np.matrix('0.0 0.0 0.0; 0.0 0.0 0.0; 0.0 0.0 0.0')
+  R_I = np.matrix('1.0 0.0 0.0; 0.0 1.0 0.0; 0 0 1')
+  w_init_v = np.array([w_init[0], w_init[1], w_init[2]])
+  v_init_v = np.array([v_init[0], v_init[1], v_init[2]])
+  w_cross_v = np.cross(w_init_v, v_init_v)  
+  w_cross_v_m[0, 0] = w_cross_v[0]
+  w_cross_v_m[1, 0] = w_cross_v[1]
+  w_cross_v_m[2, 0] = w_cross_v[2]
+  
+  theta_1st = theta
+# R_w_hat = np.matrix([[0,             -w_init[2],  w_init[1]], [w_init[2],  0,              -w_init[0]], [-w_init[1], w_init[0],   0] ])
+  # print R_w_hat
+# R_w_hat << 0,             -w_init(2, 0),  w_init(1, 0), \
+#            w_init(2, 0),  0,              -w_init(0, 0), \
+#            -w_init(1, 0), w_init(0, 0),   0;
+
+# //calculate R_w_hat_theta
+  R_w_1st = R_I + R_w_hat * sin(theta_1st) + R_w_hat * R_w_hat * (1 - cos(theta_1st));
+  tt = R_w_1st[0, 0]
+  R_ksi_1st[0, 0] =  tt
+  # print 'R_ksi_1st[0, 0]', R_ksi_1st[0, 0]
+  R_ksi_1st[0, 1] =  R_w_1st[0, 1]
+  R_ksi_1st[0, 2] =  R_w_1st[0, 2]
+
+  R_ksi_1st[1, 0] =  R_w_1st[1, 0]
+  R_ksi_1st[1, 1] =  R_w_1st[1, 1]
+  R_ksi_1st[1, 2] =  R_w_1st[1, 2]
+
+  R_ksi_1st[2, 0] =  R_w_1st[2, 0]
+  R_ksi_1st[2, 1] =  R_w_1st[2, 1]
+  R_ksi_1st[2, 2] =  R_w_1st[2, 2]
+  
+  temp = (R_I - R_w_1st) * w_cross_v_m;
+  R_ksi_1st[0, 3] = temp[0, 0]
+  R_ksi_1st[1, 3] = temp[1, 0]
+  R_ksi_1st[2, 3] = temp[2, 0]
+  R_ksi_1st[3, 3] = 1
+  return R_ksi_1st
+
+def get_pose_from_R_h(R_h):
+  new_q = tf.quaternion_from_matrix(R_h)
+  #set robot target
+  pose_target = geometry_msgs.msg.Pose()
+  pose_target.orientation.x = new_q[0]
+  pose_target.orientation.y = new_q[1]
+  pose_target.orientation.z = new_q[2]
+  pose_target.orientation.w = new_q[3]
+  pose_target.position.x = R_h[0, 3]
+  pose_target.position.y = R_h[1, 3]
+  pose_target.position.z = R_h[2, 3]
+  return pose_target
+
+def get_R_h_from_pose(pose):
+  #get homogenious matrix of initial state
+  init_q = []
+  init_q.append(pose.orientation.x)
+  init_q.append(pose.orientation.y)
+  init_q.append(pose.orientation.z)
+  init_q.append(pose.orientation.w)
+  init_R_h = tf.quaternion_matrix(init_q)
+  init_R_h[0, 3] = pose.position.x
+  init_R_h[1, 3] = pose.position.y 
+  init_R_h[2, 3] = pose.position.z
+  return init_R_h
+
+def calc_waypoints_ARC(init_pose, center_point, axis, total_angle):
+  way_points = []
+  way_points.append(copy.deepcopy(init_pose));
+
+  
+  #calculate rotate matrix
+  step_angle = 5.0 / 180 * 3.1415926
+  current_angle = step_angle
+  total_angle = total_angle / 180.0 * 3.1415926
+
+  v_init = np.cross(center_point, axis)
+  w_init = axis
+  R_h_transform = calc_R_h_from_ksi(w_init, v_init, step_angle);
+
+  current_R_h = get_R_h_from_pose(init_pose);
+  print 'current angle ', current_angle
+  print 'total angle ', total_angle
+  
+  while (current_angle <= total_angle):
+    current_R_h = R_h_transform * current_R_h 
+    new_target = get_pose_from_R_h(current_R_h)
+    way_points.append(new_target)
+    print 'add target'
+    current_angle += step_angle
+
+  return way_points
 
 
 # Gripper command functions
@@ -56,6 +156,42 @@ def gripper_close():
   rospy.sleep(1)
 
 #gripper command function end
+
+def scale_trajectory_speed(traj, scale):
+  # Create a new trajectory object
+  new_traj = RobotTrajectory()
+
+  # Initialize the new trajectory to be the same as the planned trajectory
+  new_traj.joint_trajectory = traj.joint_trajectory
+
+  # Get the number of joints involved
+  n_joints = len(traj.joint_trajectory.joint_names)
+
+  # Get the number of points on the trajectory
+  n_points = len(traj.joint_trajectory.points)
+
+  # Store the trajectory points
+  points = list(traj.joint_trajectory.points)
+
+  # Cycle through all points and scale the time from start, speed and acceleration
+  for i in range(n_points):
+     point = JointTrajectoryPoint()
+     point.time_from_start = traj.joint_trajectory.points[i].time_from_start / scale
+     point.velocities = list(traj.joint_trajectory.points[i].velocities)
+     point.accelerations = list(traj.joint_trajectory.points[i].accelerations)
+     point.positions = traj.joint_trajectory.points[i].positions
+                  
+     for j in range(n_joints):
+         point.velocities[j] = point.velocities[j] * scale
+         point.accelerations[j] = point.accelerations[j] * scale * scale
+     
+     points[i] = point
+
+  # Assign the modified points to the new trajectory
+  new_traj.joint_trajectory.points = points
+
+  # Return the new trajecotry
+  return new_traj
 
 
 def shallow_insertion_start():
@@ -103,7 +239,7 @@ def shallow_insertion_start():
 
   ## Wait for RVIZ to initialize. This sleep is ONLY to allow Rviz to come up.
   print "============ Waiting for RVIZ..."
-  rospy.sleep(5)
+  rospy.sleep(2)
   print "============ Starting tutorial "
 
   ## Getting Basic Information
@@ -123,12 +259,34 @@ def shallow_insertion_start():
   ## robot.
   print "============ Printing group state"
   print group.get_current_pose()
+  
 
+
+  # ## Then, we will get the current set of joint values for the group
+  # group_variable_values = group.get_current_joint_values()
+  # print "============ Joint values: ", group_variable_values
+
+  ## Now, let's modify one of the joints, plan to the new joint
+  ## space goal and visualize the plan
+  group_variable_values = [-0.02749187151064092, -1.598161522542135, -1.8247435728656214, -3.7606776396380823, -1.3931792418109339, 1.796911358833313]
+  group.set_joint_value_target(group_variable_values)
+  plan2 = group.plan()
+
+  print "============ Waiting while RVIZ displays plan2..."
+  group.execute(plan2)
+
+  rospy.sleep(2)
+
+
+  # moveit_commander.roscpp_shutdown()
+
+  # return
   # Initialize robot environment END
   
 
   #2. Check if the gripper works well, then open it 
   gripper_init()
+  waypoints = []  
 
 
   #3. Go to the beginning position before closing the gripper and pick up the card 
@@ -180,7 +338,11 @@ def shallow_insertion_start():
   group.execute(plan1)
   print 'point 1'
   rospy.sleep(2)
+  # center_point = [-0.748232, -0.115241, 0.150822]
 
+  center_point = [-0.77998840649, 0.00084252595, 0.369628519 - 0.315]
+  rotate_axis = [0, 1, 0]
+  waypoints_new = calc_waypoints_ARC(pose_target, center_point, rotate_axis, 45)
 
   #8. Move to the 2nd way point in the insertion process
   pose_target.orientation.x = 0.48757600855
@@ -190,11 +352,11 @@ def shallow_insertion_start():
   pose_target.position.x = -0.77884294211
   pose_target.position.y = 0.0011804883180
   pose_target.position.z = 0.37023884140
-  group.set_pose_target(pose_target)
-  plan1 = group.plan()
-  group.execute(plan1)
-  print 'point 2'
-  rospy.sleep(2)
+  # group.set_pose_target(pose_target)
+  # plan1 = group.plan()
+  # group.execute(plan1)
+  # print 'point 2'
+  # rospy.sleep(2)
 
 
   #9. Move to the 3rd way point in the insertion process
@@ -205,12 +367,12 @@ def shallow_insertion_start():
   pose_target.position.x = -0.775233949849
   pose_target.position.y = 0.00547868308962
   pose_target.position.z = 0.369664566685
-  group.set_pose_target(pose_target)
-  plan1 = group.plan()
-  group.execute(plan1)
-  print 'point 3'
-  rospy.sleep(2)
-
+  waypoints.append(copy.deepcopy(pose_target))
+  # group.set_pose_target(pose_target)
+  # plan1 = group.plan()
+  # group.execute(plan1)
+  # print 'point 3'
+  # rospy.sleep(2)
 
   #9. Move to the 4th way point in the insertion process
   pose_target.orientation.x = 0.44689637914
@@ -220,11 +382,12 @@ def shallow_insertion_start():
   pose_target.position.x = -0.73393384646
   pose_target.position.y = 0.0108484437617
   pose_target.position.z = 0.365896888399
-  group.set_pose_target(pose_target)
-  plan1 = group.plan()
-  group.execute(plan1)
-  print 'point 4'
-  rospy.sleep(2)
+  waypoints.append(copy.deepcopy(pose_target))
+  # group.set_pose_target(pose_target)
+  # plan1 = group.plan()
+  # group.execute(plan1)
+  # print 'point 4'
+  # rospy.sleep(2)
 
 
   #10. Move to the 5th way point in the insertion process
@@ -235,11 +398,12 @@ def shallow_insertion_start():
   pose_target.position.x = -0.695318810462
   pose_target.position.y = 0.0177422577652
   pose_target.position.z = 0.356604255418
-  group.set_pose_target(pose_target)
-  plan1 = group.plan()
-  group.execute(plan1)
-  print 'point 5'
-  rospy.sleep(2)
+  waypoints.append(copy.deepcopy(pose_target))
+  # group.set_pose_target(pose_target)
+  # plan1 = group.plan()
+  # group.execute(plan1)
+  # print 'point 5'
+  # rospy.sleep(2)
 
 
   #11. Move to the 6th way point in the insertion process
@@ -250,11 +414,12 @@ def shallow_insertion_start():
   pose_target.position.x = -0.662692143568
   pose_target.position.y = 0.0118211753503
   pose_target.position.z = 0.343935175687
-  group.set_pose_target(pose_target)
-  plan1 = group.plan()
-  group.execute(plan1)
-  print 'point 6'
-  rospy.sleep(2)
+  waypoints.append(copy.deepcopy(pose_target))
+  # group.set_pose_target(pose_target)
+  # plan1 = group.plan()
+  # group.execute(plan1)
+  # print 'point 6'
+  # rospy.sleep(2)
 
 
   #12. Move to the 7th way point in the insertion process
@@ -265,14 +430,41 @@ def shallow_insertion_start():
   pose_target.position.x = -0.592417814392
   pose_target.position.y = 0.010615234106
   pose_target.position.z = 0.299961240327
-  group.set_pose_target(pose_target)
-  plan1 = group.plan()
-  group.execute(plan1)
-  print 'point 7'
+  waypoints.append(copy.deepcopy(pose_target))
+  # group.set_pose_target(pose_target)
+  # plan1 = group.plan()
+  # group.execute(plan1)
+  # print 'point 7'
+  # rospy.sleep(2)
+
+
+  #execute path with waypoints
+  ## We want the cartesian path to be interpolated at a resolution of 1 cm
+  ## which is why we will specify 0.01 as the eef_step in cartesian
+  ## translation.  We will specify the jump threshold as 0.0, effectively
+  ## disabling it.
+  print 'way_points new '
+  # print waypoints_new
+
+  (plan3, fraction) = group.compute_cartesian_path(
+                               waypoints_new,   # waypoints to follow
+                               0.01,        # eef_step
+                               0.0)         # jump_threshold
+                               
+  print "============ Waiting while RVIZ displays plan3..."
+  
+
+
+  plan3_slower = scale_trajectory_speed(plan3, 0.5)
+  group.execute(plan3)
   rospy.sleep(2)
 
+  moveit_commander.roscpp_shutdown()
+
+  return  
 
   #13. Widen the gripper
+  print 'gripper 200'
   gripper_200()
 
 
@@ -325,7 +517,7 @@ def shallow_insertion_start():
   rospy.sleep(2)
 
 
-   #18. Move to the 10th way point in the insertion process
+   #18. Move to the 11th way point in the insertion process
   pose_target.orientation.x = 0.364964183344
   pose_target.orientation.y = 0.628658362491
   pose_target.orientation.z = -0.580710320412
@@ -336,11 +528,11 @@ def shallow_insertion_start():
   group.set_pose_target(pose_target)
   plan1 = group.plan()
   group.execute(plan1)
-  print 'point 10'
+  print 'point 11'
   rospy.sleep(2)
 
 
-  #19. Move to the 11th way point in the insertion process
+  #19. Move to the 12th way point in the insertion process
   pose_target.orientation.x = 0.396925730678
   pose_target.orientation.y = 0.583354341151
   pose_target.orientation.z = -0.680227755847
@@ -351,7 +543,7 @@ def shallow_insertion_start():
   group.set_pose_target(pose_target)
   plan1 = group.plan()
   group.execute(plan1)
-  print 'point 11'
+  print 'point 12'
   rospy.sleep(2)
 
 
@@ -370,5 +562,4 @@ if __name__=='__main__':
     shallow_insertion_start()
   except rospy.ROSInterruptException:
     pass
-
 
